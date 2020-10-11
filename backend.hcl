777ca2dd0820679d5375b4bc14631afd962e8fd4
Kuze Yang <greyson.y.it@gmail.com>
2020-10-11 23:45:59 +1100

    Initial commit for terraform s3 backend

---
+++ backend.hcl
@@ -0,0 +1,4 @@
+bucket = "greyson-terraform-state"
+region = "ap-southeast-2"
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt = true
---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "greyson-terraform-state"
+
+  lifecycle {
+      prevent_destroy = true
+  }
+
+  versioning {
+      enabled = true
+  }
+
+  server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+          }
+      }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+output "s3_bucket_arn" {
+    value = aws_s3_bucket.terraform_state.arn
+    description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+    value = aws_dynamodb_table.terraform_locks.name
+    description = "The name of the DynamoDB table"
+}
+
+terraform {
+    backend "s3" {
+        key = "global/s3/terraform.tfstate"
+    }
+}
